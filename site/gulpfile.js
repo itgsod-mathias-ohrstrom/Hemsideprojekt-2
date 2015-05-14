71e2eb024886ef87600a164106f39ea7933ab1b2
itgsod-mathias-ohrstrom <mathias.ohrstrom@elev.it-gymnasiet.se>
2015-05-14 22:25:01 +0200

    byt så bilderna nur är i css sprite

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
-var concat = require('gulp-concat-limit');
+var concatCss = require('gulp-concat-css');
 var gulpif = require('gulp-if');
 var sprite = require('css-sprite').stream;
 var minifyHTML = require('gulp-minify-html');
@@ -10,7 +10,6 @@ var minifyHTML = require('gulp-minify-html');
 
 
 gulp.task('default', function() {
-  // place code for your default task here
 
 });
 
@@ -19,12 +18,12 @@ gulp.task('default', function() {
 gulp.task('compress', function() {
   return gulp.src('bootstrap-3.3.2-dist/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist/js/'));
 });
 
 
 /*CSS Sprite map generator*/
-gulp.task('sprites', function () {
+gulp.task('sprites', function() {
   return gulp.src('./img/*.png')
     .pipe(sprite({
       name: 'sprite',
@@ -32,15 +31,15 @@ gulp.task('sprites', function () {
       cssPath: './img',
       processor: 'css'
     }))
-    .pipe(gulpif('*.png', gulp.dest('./dist/img/'), gulp.dest('./dist/scss/')))
+    .pipe(gulpif('*.png', gulp.dest('./img/'), gulp.dest('./css/')));
 });
 
 
 /*CSS Concat*/
-gulp.task('concat', function() {
-    gulp.src('./css/*.css')
-        .pipe(concat('style-.css', 256))//2KB
-        .pipe(gulp.dest('./combinedCSS'));
+gulp.task('default', function() {
+  return gulp.src('./**/*.css')
+    .pipe(concatCss("styles/bundle.css"))
+    .pipe(gulp.dest('out/'));
 });
 
 
@@ -48,18 +47,20 @@ gulp.task('concat', function() {
 /*CSS Minifyer*/
 gulp.task('minify-css', function() {
   return gulp.src('./css/*.css')
-    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(minifyCss({
+      compatibility: 'ie8'
+    }))
     .pipe(gulp.dest('dist'));
 });
- 
 
- /*html minifyer*/
+
+/*HTML minifyer*/
 gulp.task('minify-html', function() {
   var opts = {
     conditionals: true,
-    spare:true
+    spare: true
   };
- 
+
   return gulp.src('./*.html')
     .pipe(minifyHTML(opts))
     .pipe(gulp.dest('./dist/'));
